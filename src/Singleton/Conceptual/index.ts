810809eb2e022b8deb65f8ed27c526b33f660ab6
Moritz Tim Widmann <90388353+moritz-t-w@users.noreply.github.com>
2024-05-13 14:10:35 +0200

    enforce 80 columns line width limit

--- src/Singleton/Conceptual/index.ts
+++ src/Singleton/Conceptual/index.ts
@@ -70,7 +70,9 @@ function clientCode() {
     const s2 = Singleton.getInstance();
 
     if (s1 === s2) {
-        console.log('Singleton works, both variables contain the same instance.');
+        console.log(
+            'Singleton works, both variables contain the same instance.'
+        );
     } else {
         console.log('Singleton failed, variables contain different instances.');
     }
